7d7e4d8e2f00979879a688ac0ef0eb2b330ac1a9
ramaalmoamni1 <raalmomani17197@eng.just.edu.jo>
2023-09-14 13:29:44 +0000

    almost

--- src/Components/Context/Settings/Settings.jsx
+++ src/Components/Context/Settings/Settings.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useReducer } from "react";
 import { stateReducer, initialState } from "../../../hooks/Reducer/reducer";
-import Auth from "../../auth/Auth";
 export const SettingContext = React.createContext();
 export default function Settings({ children }) {
   const [settings, dispatch] = useReducer(stateReducer, initialState, () => {
